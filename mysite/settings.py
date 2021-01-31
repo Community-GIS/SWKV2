9699de59429a4e547ffdb94ef23da5956837f3ec
Monika <monikapatira@gmail.com>
2021-01-31 15:16:22 +0530

    MAP Updated, FAQ Added

--- mysite/settings.py
+++ mysite/settings.py
@@ -192,5 +192,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'monikapatira@gmail.com'#sender's email-id 
-EMAIL_HOST_PASSWORD ='ttmbakgnpamkkj' #password associated with above email-id 
+EMAIL_HOST_PASSWORD ='' #password associated with above email-id 
 
