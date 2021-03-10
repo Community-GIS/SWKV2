d63fdd696fd23a1bf9a992d7d10ca8b8dea02560
Unnati Mishra <unnatimishra15@gmail.com>
2021-03-10 20:42:28 +0530

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,10 +123,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'swk',
-        'USER': 'swkuser',
-        'PASSWORD': 'swkpass',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
